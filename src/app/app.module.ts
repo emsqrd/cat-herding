9605c698d1793ed318da535835b3f82f4104da19
Mark <madlyinanger37@gmail.com>
2023-11-11 10:12:05 -0600

    Rename create request leave component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateRequestComponent } from './components/create-request/create-request.component';
+import { CreateRequestComponent } from './components/create-leave-request/create-leave-request.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LeaveRequestsListComponent } from './components/leave-requests-list/leave-requests-list.component';
 
