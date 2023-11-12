ab123542e02ff665e753efe7f2d2566cf8c8178b
Mark <madlyinanger37@gmail.com>
2023-11-12 08:53:20 -0600

    Prettier changes

--- src/app/components/create-leave-request/create-leave-request.component.ts
+++ src/app/components/create-leave-request/create-leave-request.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { LeaveType } from '../../interfaces/leave-type';
 import { LeaveRequestsService } from '../../services/leave-requests.service';
 import { LeaveRequest } from '../../interfaces/leave-request';
@@ -10,47 +15,48 @@ import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'ch-create-leave-request',
   templateUrl: './create-leave-request.component.html',
-  styleUrl: './create-leave-request.component.scss'
+  styleUrl: './create-leave-request.component.scss',
 })
-
-export class CreateRequestComponent { 
-
+export class CreateRequestComponent {
   leaveRequestForm: FormGroup = new FormGroup({});
   leaveTypes: LeaveType[] = [];
-  
-  initialState: BehaviorSubject<LeaveRequest> = new BehaviorSubject(new LeaveRequestModel());
+
+  initialState: BehaviorSubject<LeaveRequest> = new BehaviorSubject(
+    new LeaveRequestModel()
+  );
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
-    private leaveRequestsService: LeaveRequestsService) { }
-  
+    private leaveRequestsService: LeaveRequestsService
+  ) {}
+
   getSubmitTypes(): void {
-    this.leaveRequestsService.getLeaveTypes()
-      .subscribe(leaveTypes => this.leaveTypes = leaveTypes);
+    this.leaveRequestsService
+      .getLeaveTypes()
+      .subscribe((leaveTypes) => (this.leaveTypes = leaveTypes));
   }
 
   onSubmit(): void {
-    this.leaveRequestsService.createLeaveRequest(this.leaveRequestForm.value)
+    this.leaveRequestsService
+      .createLeaveRequest(this.leaveRequestForm.value)
       .subscribe({
         next: () => {
           console.log(this.leaveRequestForm.value);
           this.router.navigate(['/leave-request-list']);
-        }
+        },
       });
   }
 
   ngOnInit() {
     this.getSubmitTypes();
-    
-    this.initialState.subscribe(leaveRequest => {
+
+    this.initialState.subscribe((leaveRequest) => {
       this.leaveRequestForm = this.fb.group({
         description: [leaveRequest.description],
         leaveType: [leaveRequest.leaveType],
         leaveDate: [leaveRequest.leaveDate],
       });
-    })
-  
+    });
   }
-
 }
